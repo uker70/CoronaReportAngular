e2902935b53f8d5df58238cfcf291010a60171da
Benjamin Damkjær Østner <benj4539@zbc.dk>
2020-08-18 14:34:20 +0200

    update
    Co-authored-by: b.r.deleuran@hotmail.com

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -24,18 +24,50 @@ export class DataComponent implements OnInit {
   mapsData: MapsData[] = [];
 
   constructor(private http: HttpService) { }
+
+  combineNumbers(a: number, b: number){
+    return (a + b);
+  }
   
   ngOnInit() {
     this.http.GetCoronaData().subscribe(data => {
       data.forEach(c => {
           //console.log(c.country);
 
-          this.coronaData.push(new Country(c.country, c.cases, c.todayCases, c.deaths, c.todayDeaths,
-            c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          if(c.country == "CAR")
+          {
+            this.coronaData.push(new Country("Central African Republic", c.cases, c.todayCases, c.deaths, c.todayDeaths,
+              c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          }
+          else if (c.country == "UAE")
+          {
+            this.coronaData.push(new Country("United Arab Emirates", c.cases, c.todayCases, c.deaths, c.todayDeaths,
+              c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          }
+          else if (c.country == "UK")
+          {
+            this.coronaData.push(new Country("United Kingdom", c.cases, c.todayCases, c.deaths, c.todayDeaths,
+              c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          }
+          else if (c.country == "DRC")
+          {
+            this.coronaData.push(new Country("Democratic Republic of the Congo", c.cases, c.todayCases, c.deaths, c.todayDeaths,
+              c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          }
+          else if (c.country == "S. Korea")
+          {
+            this.coronaData.push(new Country("South Korea", c.cases, c.todayCases, c.deaths, c.todayDeaths,
+              c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          }
+          else
+          {
+            this.coronaData.push(new Country(c.country, c.cases, c.todayCases, c.deaths, c.todayDeaths,
+              c.recovered, c.active, c.critical, c.totalTests, c.casesPerOneMillion, c.deathsPerOneMillion, c.testsPerOneMillion));
+          }
       });
     });
 
-    this.http.GetCountryData().subscribe(data => {
+    /*this.http.GetCountryData().subscribe(data => {
       data.forEach(m => {
         console.log(m.name);
 
@@ -45,7 +77,7 @@ export class DataComponent implements OnInit {
           }
         })
       });
-    });
+    });*/
 
   }
 }
